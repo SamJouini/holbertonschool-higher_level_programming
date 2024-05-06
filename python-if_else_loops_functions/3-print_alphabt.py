9913610aa6141f63c739e3b71b34cbc6c9a832a1
SamJouini <samantha.u.h.jouini@gmail.com>
2024-05-06 14:59:57 +0200

    fix whitespaces

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 for letter in range(ord('a'), ord('z') + 1):
-    if chr(letter) != 'q' and chr(letter) != 'e':
-        print('{}'.format(chr(letter)), end="")
-        
+	if chr(letter) != 'q' and chr(letter) != 'e':
+		print('{}'.format(chr(letter)), end="")
