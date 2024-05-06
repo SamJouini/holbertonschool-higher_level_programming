0f3857b496769f3a0c5779f5c6b47e1f69a439a0
SamJouini <samantha.u.h.jouini@gmail.com>
2024-05-06 13:51:08 +0200

    indentation

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python3
-import random
-number = random.randint(-10, 10)
-if number > 0:
-print(f"{number} is positive")
-else if number == 0:
-print(f"{number} is zero")
-else:
-print(f"{number} is negative")
+    #!/usr/bin/python3
+    import random
+    number = random.randint(-10, 10)
+
+    if number > 0:
+    print(f"{number} is positive")
+    else if number == 0:
+    print(f"{number} is zero")
+    else:
+    print(f"{number} is negative")
