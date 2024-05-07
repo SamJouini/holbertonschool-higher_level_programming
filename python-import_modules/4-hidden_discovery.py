af00587cd987171c16a3de8ae6969ffadff749b7
SamJouini <samantha.u.h.jouini@gmail.com>
2024-05-07 12:45:26 +0200

    fix : 2

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 import hidden_4
 
 if __name__ == "__main__":
     for hidden in dir(hidden_4):
-        if hidden[0] != "_":
+        if hidden[:2] != "_":
             print(hidden)
