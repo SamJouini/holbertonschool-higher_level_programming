332a7aa90c73ecabccedd9f42f6e9f9bab31dc1d
SamJouini <samantha.u.h.jouini@gmail.com>
2024-05-07 12:30:29 +0200

    addition

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,14 +3,14 @@
 from sys import argv
 
 if __name__ == "__main__":
-    len_argv = len(argv) - 1
+    num_argv = len(argv) - 1
 
-    if len_argv == 0:
+    if num_argv == 0:
         print("0 arguments.")
-    elif len_argv == 1:
+    elif num_argv == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(len_argv))
+        print("{} arguments:".format(num_argv))
 
     for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
