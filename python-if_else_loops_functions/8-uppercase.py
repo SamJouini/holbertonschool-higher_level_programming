bfc3bea3751f9af4d8e4b363b28451544c251306
SamJouini <samantha.u.h.jouini@gmail.com>
2024-05-07 10:02:05 +0200

    is upper

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    result = ""
+    for char in str:
+        if 97 <= ord(char) <= 122:
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    print(result)
