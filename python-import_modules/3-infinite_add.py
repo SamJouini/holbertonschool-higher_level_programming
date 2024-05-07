750074e4a2830afdd0d28bcf058cbf5f1c91536f
SamJouini <samantha.u.h.jouini@gmail.com>
2024-05-07 12:31:29 +0200

    addition

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+if __name__ == "__main__":
+    num_argv = len(argv) - 1
+    total = 0
+
+for arg in sys.argv[1:]:
+        total += int(arg)
+        print(total)
