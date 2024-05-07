5010a9e060bed8849ba3f0626c93637811f1ca25
SamJouini <samantha.u.h.jouini@gmail.com>
2024-05-07 12:10:20 +0200

    argument

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,8 +4,8 @@ import sys
 
 if __name__ == "__main__":
     num_args = len(sys.argv) - 1
-    print(f"Number of argument{'s' if num_args != 1 else ''}: {num_args}")
+    print(f"{'s' if num_args != 1 else ''} argument: {num_args}")
     print(": " if num_args == 0 else ":")
 
     for i, arg in enumerate(sys.argv[1:], start=1):
-        print(f"{i} argument: {arg}")
+        print(f"{i}: {arg}")
