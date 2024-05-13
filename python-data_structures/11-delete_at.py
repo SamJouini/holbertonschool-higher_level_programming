ebd3d924127743a0169533a3659c85914544b1f4
SamJouini <samantha.u.h.jouini@gmail.com>
2024-05-13 12:06:33 +0200

    fix : name

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -4,5 +4,5 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
 
-    del new_list[idx]
-    return new_list
+    del my_list[idx]
+    return my_list
