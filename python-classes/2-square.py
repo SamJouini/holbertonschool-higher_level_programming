842668ce4dc7e2228065b3e0bf71bb28847a0805
SamJouini <samantha.u.h.jouini@gmail.com>
2024-05-14 14:17:03 +0200

    maybe

---
+++ python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"Defines a square with a private size attribute and validation."
+
+
+class Square:
+    def __init__(self, size=0):
+        "Initializes a Square instance with a given size."
+        if not isinstance(size, int):
+            "Check if size is an integer"
+            raise TypeError("size must be an integer")
+            "If `size` is not an integer."
+        if size < 0:
+            "If `size` is less than 0."
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        "If size is valid, set the private __size attribute"
