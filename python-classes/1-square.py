2040b9075875b61250aed1e1fe95c4ed0bad51bf
SamJouini <samantha.u.h.jouini@gmail.com>
2024-05-14 14:01:34 +0200

    fix : pass

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,8 +2,10 @@
 "Defines a square with a private size attribute."
 
 
-class Square:
+class Square():
     def __init__(self, size):
         "Initializes a Square instance with a given size."
         self.__size = size
         "Set the private __size attribute to the provided size"
+
+    pass
