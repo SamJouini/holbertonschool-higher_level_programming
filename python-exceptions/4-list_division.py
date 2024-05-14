08ddcc2e5013f7d7c5349d56a32d79640501d86d
SamJouini <samantha.u.h.jouini@gmail.com>
2024-05-14 11:28:55 +0200

    fix typo

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -6,7 +6,7 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
-            print("division by O")
+            print("division by 0")
             result = 0
         except (ValueError, TypeError):
             print("wrong type")
