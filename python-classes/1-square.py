b9cc5aa8ba94b6d9c27babb1713c92b9d8f43dea
SamJouini <samantha.u.h.jouini@gmail.com>
2024-05-14 14:19:20 +0200

    fix : doc

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Defines a square with a private size attribute."""
 
 
 class Square():
+    "Defines a square with a private size attribute."
     def __init__(self, size):
-        """Initializes a Square instance with a given size."""
+        "Initializes a Square instance with a given size."
         self.__size = size
-        """Set the private __size attribute to the provided size"""
+        "Set the private __size attribute to the provided size"
