9555c7067f9c300dc283652633f8cb1732019f76
SamJouini <samantha.u.h.jouini@gmail.com>
2024-05-14 13:46:32 +0200

    fix: documentation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-class Square:
-    """Defines a square.
+"Defines a square in Python."
 
+
+class Square:
     Attributes:
+        "Class is curently empty."
         None
-    """
-    pass
