e0d49da40551192eb44c29466ba656957813f9e6
SamJouini <samantha.u.h.jouini@gmail.com>
2024-05-15 10:41:12 +0200

    singly linked list

---
+++ python-classes/100-singly_linked_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+class Node:
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
