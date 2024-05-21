f63cd8153507e6a4d1aa9a0b188a3f245e2217dd
SamJouini <samantha.u.h.jouini@gmail.com>
2024-05-21 13:45:41 +0200

    fix: missed modif

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -4,10 +4,12 @@
 
 class Rectangle:
     "Initializes a Rectangle instance"
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
