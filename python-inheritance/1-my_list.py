e23af681e4b5299bd09739642ea38dbec069ac6a
SamJouini <samantha.u.h.jouini@gmail.com>
2024-05-22 13:40:25 +0200

    fix: typo

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,16 +4,13 @@ A class that inherits from the built-in list class and
 provides a method to print the list in sorted order.
 """
 
+
 class MyList(list):
     "Prints the list in ascending sorted order."
 
     def print_sorted(self):
 
         sorted_list = sorted(self)
-        print(sorted_list)
         "Create a new sorted list from the elements of the current list"
-
-        sorted_list = sorted(self)
-
         print(sorted_list)
         "Print the sorted list"
