b1207861ab5e64794471fbf85a0f443811d87277
SamJouini <samantha.u.h.jouini@gmail.com>
2024-05-22 13:59:47 +0200

    base geo

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+An empty base class for geometry-related classes.
+"""
+
+
+class BaseGeometry:
+    """
+    This class serves as a foundation for other
+    geometry classes to inherit from.
+    It doesn't have any attributes or methods defined within it.
+    """
+    pass
