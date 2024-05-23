e7a15b662e65385feeb4d739234a60103e69949f
SamJouini <samantha.u.h.jouini@gmail.com>
2024-05-23 11:13:29 +0200

    fix: space

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
 """
-Prints a text with 2 new lines after each of the characters: ., ? and :"""
+Prints a text with 2 new lines after each of the characters: ., ? and :
+"""
 
 
 def text_indentation(text):
-    "TypeError: If text is not a string."
+    """
+    Prints a text with 2 new lines
+    after each of these characters: `.`, `?` and `:`.
+
+    Args:
+    text : The input text to be print.
+
+    Raises:
+    TypeError: If the input `text` is not a string.
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
@@ -13,7 +23,7 @@ def text_indentation(text):
     for char in text:
         new_text += char
         if char in characters:
-            new_text += "\n\n"
+            new_text += "\n"
 
     lines = new_text.split("\n")
     for line in lines:
