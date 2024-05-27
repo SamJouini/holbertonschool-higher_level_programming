95033cd200c89912236d431340666dc7d33d2986
SamJouini <samantha.u.h.jouini@gmail.com>
2024-05-27 15:29:55 +0200

    fix: wrong push

--- python-input_output/8-my_class.py
+++ python-input_output/8-my_class.py
@@ -10,17 +10,8 @@ def class_to_json(obj):
     """
     Returns the dictionary description with simple data structures
     for JSON serialization of an object.
-
-    Args:
-        obj: An instance of a class.
-
+    
     Returns:
         dict: A dictionary representation of the object's attributes.
     """
-    obj_dict = {}
-    for attr, value in vars(obj).items():
-        if isinstance(value, (list, dict, str, int, bool)):
-            obj_dict[attr] = value
-        else:
-            obj_dict[attr] = str(value)
-    return obj_dict
+    return obj.__dict__
