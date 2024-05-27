f1a5060a3bc6e0691c507776ad7b09e5ef841ee0
SamJouini <samantha.u.h.jouini@gmail.com>
2024-05-27 11:26:47 +0200

    fix: req

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -8,12 +8,5 @@ def read_file(filename=""):
         filename (str): The path to the text file.
         If not provided, an empty string is used.
     """
-    if not filename:
-        return
-
-    try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            contents = file.read()
-            print(contents)
-    except Exception as e:
-        print("Error: {}".format(e))
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.read(), end="")
