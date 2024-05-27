11bc4197a07047191df1bba912fd5db192b78ae8
SamJouini <samantha.u.h.jouini@gmail.com>
2024-05-27 15:00:17 +0200

    my class

---
+++ python-input_output/8-my_class.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Function that returns the dictionary description
+with simple data structure (list, dictionary, string, integer and boolean)
+for JSON serialization of an object.
+"""
+
+
+def class_to_json(obj):
+    """
+    Returns the dictionary description with simple data structures
+    for JSON serialization of an object.
+
+    Args:
+        obj: An instance of a class.
+
+    Returns:
+        dict: A dictionary representation of the object's attributes.
+    """
+    obj_dict = {}
+    for attr, value in obj.__dict__.items():
+        if isinstance(value, (list, dict, str, int, bool)):
+            obj_dict[attr] = value
+        else:
+            obj_dict[attr] = str(value)
+    return obj_dict
