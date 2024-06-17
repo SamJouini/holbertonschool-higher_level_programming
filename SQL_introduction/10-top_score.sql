36a4f7c2fa1feda8eb7ae2ebbdd5a5675a79a09f
SamJouini <samantha.u.h.jouini@gmail.com>
2024-06-17 13:21:51 +0200

    tasks

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,2 @@
+-- List all records from the table second_table ordered by score
+SELECT score, name FROM second_table ORDER BY score DESC;
---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- Count the number of records with id = 89 in the table first_table
+SELECT COUNT(*) AS count FROM first_table WHERE id = 89;
