387f2ac33dea1447ec12685f619efa184192ec4d
SamJouini <samantha.u.h.jouini@gmail.com>
2024-06-17 14:55:32 +0200

    fixed

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,12 +1,3 @@
--- Switch to the MySQL database
-USE mysql;
-
--- List privileges for user_0d_1
-SELECT Host, User, Db, Table_name, Table_priv, Column_priv
-FROM mysql.tables_priv
-WHERE User = 'user_0d_1';
-
--- List privileges for user_0d_2
-SELECT Host, User, Db, Table_name, Table_priv, Column_priv
-FROM mysql.tables_priv
-WHERE User = 'user_0d_2';
+--List global privileges for user_0d_1, user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@localhost;
+SHOW GRANTS FOR 'user_0d_2'@localhost;
