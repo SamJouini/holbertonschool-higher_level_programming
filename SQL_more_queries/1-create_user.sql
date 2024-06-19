9714f27a6c0ce690e4e189493a4b5dd471c2d02e
SamJouini <samantha.u.h.jouini@gmail.com>
2024-06-19 11:21:18 +0200

    fixed

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,6 @@
+-- -- Creates the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
 -- Create the user if it doesn't exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
