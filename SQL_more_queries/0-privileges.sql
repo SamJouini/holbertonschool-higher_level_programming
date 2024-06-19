de38b814cea408e24effd0ef706c4bb07977e096
SamJouini <samantha.u.h.jouini@gmail.com>
2024-06-19 10:11:52 +0200

    test

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 --List global privileges for user_0d_1, user_0d_2
-SHOW GRANTS FOR 'user_0d_1'@localhost;
-SHOW GRANTS FOR 'user_0d_2'@localhost;
+SHOW GRANTS FOR 'user_0d_1';
+SHOW GRANTS FOR 'user_0d_2';
