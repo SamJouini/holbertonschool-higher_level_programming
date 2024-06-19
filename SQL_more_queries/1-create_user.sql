9dba0157e47570ad77a6fa5bbd28e7de31f80fbb
SamJouini <samantha.u.h.jouini@gmail.com>
2024-06-19 11:23:27 +0200

    fixed

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -4,5 +4,5 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Create the user if it doesn't exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
--- Grant all privileges to the user
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+-- Grant privileges to the user
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
