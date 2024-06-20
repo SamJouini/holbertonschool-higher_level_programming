4c30a2cbbe92162f99cd981559ad652f535821e1
SamJouini <samantha.u.h.jouini@gmail.com>
2024-06-20 18:25:56 +0200

    test

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,3 @@
--- -- Creates the database if it doesn't exist
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-
--- Create the user if it doesn't exist
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY "user_0d_2_pwd";
-
--- Grant privileges to the user
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+-- Create MySQL user user_0d_1 with password user_0d_1_pwd
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
