2476ed36ef6b87921af3344c8bcf40bf3cb972b6
SamJouini <samantha.u.h.jouini@gmail.com>
2024-06-20 13:28:51 +0200

    fix: pyco

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 
-#   Lists all states from the database hbtn_0e_0_usa
+# Lists all states from the database hbtn_0e_0_usa
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-      # Connect to the MySQL server
+    # Connect to the MySQL server
     db = MySQLdb.connect(host="localhost",
-                        user=sys.argv[1],
-                        passwd=sys.argv[2],
-                        db=sys.argv[3])
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
 
     # Create a cursor object
     cursor = db.cursor()
