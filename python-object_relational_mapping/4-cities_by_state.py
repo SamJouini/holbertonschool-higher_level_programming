dfaf12b215829b6ff8925201093684313a8b836c
SamJouini <samantha.u.h.jouini@gmail.com>
2024-06-21 14:55:13 +0200

    test

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,35 +1,22 @@
 #!/usr/bin/python3
-"""
-Script that lists all cities from the database hbtn_0e_4_usa
-"""
+"""takes in an argument and displays all values in the states table
+but injection safe"""
+
 
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    """ Establish a connection to the MySQL server """
-    db = MySQLdb.connect(
-        host="localhost",
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3])
 
-    """ Create a cursor object to execute SQL queries """
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-
-    """ Execute the SQL query to fetch all cities """
-    cursor.execute("SELECT cities.id, cities.name, states.name"
-                   "FROM cities"
-                   "JOIN states ON cities.state_id = states.id"
-                   "ORDER BY cities.id ASC")
-
-    """ Fetch all the rows """
-    cities = cursor.fetchall()
-
-    """ Display the results """
-    for city in cities:
-        print(city)
-
-    """ Close the cursor and database connection """
+    cursor.execute("SELECT cities.id, cities.name, states.name "
+                   "FROM cities JOIN states ON cities.state_id = states.id "
+                   " ORDER BY cities.id ")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
     cursor.close()
     db.close()
