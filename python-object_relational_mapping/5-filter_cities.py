1ebe0ba8957e4f89e078a63b266a3af73b477f51
SamJouini <samantha.u.h.jouini@gmail.com>
2024-06-21 14:56:09 +0200

    test

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,40 +1,25 @@
 #!/usr/bin/python3
-"""
-Script that lists all cities of a given state from the database hbtn_0e_4_usa
-"""
+"""takes in the name of a state as an argument
+lists all cities of that state,"""
+
 
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    """ Establish a connection to the MySQL server """
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-    )
 
-    """  Create a cursor object to execute SQL queries """
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-
-    """ Get the state name from the fourth command-line argument """
     state_name = sys.argv[4]
-
-    """ Execute the SQL query to fetch cities of the given state """
-    cursor.execute("SELECT cities.name"
-             "FROM cities "
-             "JOIN states ON cities.state_id = states.id"
-             "WHERE states.name = %s"
-             " ORDER BY cities.id ASC", (state_name,))
-  
-    """ Fetch all the rows """
+    cursor.execute("SELECT cities.name, states.name "
+                   "FROM cities "
+                   "JOIN states ON cities.state_id = states.id "
+                   "WHERE states.name = %s"
+                   " ORDER BY cities.id ASC", (state_name,))
     inception_of_rows = rows = cursor.fetchall()
-
-    """ Display the results """
     print(", ".join(row[0] for row in inception_of_rows))
 
-    """ Close the cursor and database connection """
     cursor.close()
     db.close()
