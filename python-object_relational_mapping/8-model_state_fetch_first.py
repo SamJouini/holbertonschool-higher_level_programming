5b6ac658692c0cf6f86eb68f7e4f0d8e2ad04e7e
SamJouini <samantha.u.h.jouini@gmail.com>
2024-06-21 13:27:45 +0200

    model state fetch

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -10,8 +10,8 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     """ Get MySQL connection parameters from command line arguments """
-    username = sys.argv[1],
-    password = sys.argv[2],
+    username = sys.argv[1]
+    password = sys.argv[2]
     database = sys.argv[3]
 
     """ Create engine to connect to MySQL server """
