1f456c537dc7481e7d83dbfe8b4b999a7db318ab
SamJouini <samantha.u.h.jouini@gmail.com>
2024-06-21 13:42:20 +0200

    fixed?

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -19,13 +19,16 @@ if __name__ == "__main__":
     """  Create a cursor object to execute SQL queries """
     cursor = db.cursor()
 
+    """ Get the state name from the fourth command-line argument """
+    state_name = sys.argv[4]
+
     """ Execute the SQL query to fetch cities of the given state """
     query = ("SELECT cities.name"
              "FROM cities"
              "JOIN states ON cities.state_id = states.id"
              "WHERE states.name = %s"
-             "ORDER BY cities.id ASC")
-    cursor.execute(query, (sys.argv[4]))
+             " ORDER BY cities.id ASC", (state_name,))
+    cursor.execute(query)
 
     """ Fetch all the rows """
     cities = cursor.fetchall()
